9f21cff9d5bf1fe6a19860b0dab31098436d76bb
Robi-Rozali <rozalirobi@gmail.com>
2020-11-19 13:55:55 +0700

    view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,160 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="vendor/fontawesome-free/css/all.min.css" type="text/css">
+
+    <title>Sistem Informasi Akademik STMIK</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-siakad sticky-top shadow-sm">
+      <div class="container-fluid">
+        <a href="#" class="navbar-brand">
+          <img src="img/logo.png" class="d-inline" style="width: 200px" alt="logo">
+        </a>
+      <button class="navbar-toggler navbar-light" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <div class="tulisan lead">Fake Login</div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container section1">
+      <div class="row">
+        <div class="col-md-7">
+          <div class="col-md-0 mb-3 order-md-1 mt-5">
+            <h1 class="mb-3 text-left judul">Sistem Informasi Akademik Stmik Sumedang</h1>
+            <p class="lead">Pengelolaan Sistem KRS & KHS</p>
+						<div class="row">
+					
+						</div>
+					</div>
+        </div>
+        <div class="col-md-5">
+          <div class="card shadow w-100 mt-5">
+            <div class="card-body text-center ">
+              <form method="post" action="dashboard.html" class="user">
+                <div class="form-group mt-4">
+                    <div class="text-left">
+                    <input type="text" class="form-control form-control-user " id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Masukan NPM Anda" name="username">
+                </div>
+                <div class="form-group text-left">
+                    <input type="text" class="form-control form-control-user mt-4" id="exampleInputPassword" placeholder="Masukan Password Anda" name="password">
+                </div>
+                <div class="rememberpass text-left">
+                  <input type="checkbox" name="rememberusername" id="rememberusername" value="1"  />
+                  <label for="rememberusername">Remember username</label>
+                </div>
+                  <button type="submit" class="btn btn-primary btn-block" id="loginbtn">Log in</button>
+                </div>
+            </div>
+          </div>
+      </div>
+      </div>
+    </div>
+
+    <div class="text-center mt-5 p-md-4 container-fluid" >
+      <div class="row">
+          <div class="col-md-3">
+          <div class="card mb-2 w-100 bg-utama">
+            <i class="fas fa-globe-americas fa-4x mt-3"></i>
+              <div class="card-body">
+                <p class="lead mt-3">Menghasilkan tenaga yang unggul dalam bidang teknologi informasi dan komunikasi.</p>
+              </div>
+          </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="card mb-2 w-100 h-96 bg-utama">
+              <i class="fa fa-graduation-cap fa-4x mt-3"></i>
+                <div class="card-body">
+                  <p class="lead mt-3">Lulusan di bekali dengan kemampuan softskill dan hardskill yang memadai.</p>
+                </div>
+            </div>
+            </div>
+
+          <div class="col-md-3">
+            <div class="card mb-2 w-100 bg-utama">
+              <i class="fa fa-bullhorn fa-4x mt-3"></i>
+                <div class="card-body">
+                  <p class="lead mt-3">Mengembangkan teknologi informasi dan komunikasi dalam rangka mendorong pembangunan Jawa Barat</p>
+                </div>
+            </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="card mb-2 w-100 bg-utama">
+              <i class="fa fa-mobile fa-4x mt-3"></i>
+                <div class="card-body">
+                  <p class="lead mt-3">Menghasilkan produk-produk dan kegiatan-kegiatan dari penelitian dan pengabdian pada masyarakat.</p>
+                </div>
+            </div>
+          </div>
+        </div>
+  </div>
+
+  <div class="container-fluid bg-utama">
+    <div class="row">
+      <div class="col-md-3 my-5">
+        <img src="img/logo.png" class="d-inline" style="width: 200px" alt="logo">
+        <p class="lead mt-3 tulisan">STMIK Sumedang menjadi Sekolah Tinggi yang unggul dalam bidang manajemen informatika dan komputer di Jawa Barat pada tahun 2022</p>
+      </div>
+      <div class="col-md-3 my-5">
+        <h5 class="lead">Quick Links </h5>
+        <ul style="list-style: none">
+          <li><a href="https://stmik-sumedang.ac.id/" class="teks-link"><i class="fas fa-link"></i> STMIK Sumedang</a></li>
+          <li><a href="https://ejournal.stmik-sumedang.ac.id/" class="teks-link"><i class="fas fa-link"></i> E-Journal</a></li>
+          <li><a href="https://perpus.stmik-sumedang.ac.id/" class="teks-link"><i class="fas fa-link"></i> Perpustakaan</a></li>
+          <li><a href="https://bpm.stmik-sumedang.ac.id/" class="teks-link"><i class="fas fa-link"></i> SPMI STMIK Sumedang</a></li>
+          <li><a href="https://pmb.stmik-sumedang.ac.id/" class="teks-link"><i class="fas fa-link"></i> PMB STMIK Sumedang</a></li>
+          <li><a href="https://literasidigital.id/" class="teks-link"><i class="fas fa-link"></i> Literasi Digital</a></li>
+        </ul>
+      </div>
+      <div class="col-md-3 my-5">
+        <h5 class="lead">Follow Us</h5>
+        <ul style="list-style: none">
+          <li><a href="https://facebook.com/stmiksmd" class="teks-link"><i class="fab fa-facebook fa-1x"></i> Facebook</a></li>
+          <li><a href="https://twitter.com/stmiksumedang" class="teks-link"><i class="fab fa-twitter fa-1x"></i> Twitter</a></li>
+        </ul>
+      </div>
+      <div class="col-md-3 my-5">
+        <h5 class="lead">Contact Us</h5>
+        <ul style="list-style: none">
+          <li class="tulisan"><i class="fas fa-map fa-1x"></i> Jl. Ankrek No. 19 Sumedang</li>
+          <li class="tulisan"><i class="fas fa-phone-square-alt fa-1x"></i> Phone: (0261) 207395</li>
+          <li><a href="mailto:info@stmik-sumedang.ac.id" class="teks-link" ><i class="fas fa-envelope fa-1x"></i> info@stmik-sumedang.ac.id </a></li>
+        </ul>
+      </div>
+    </div>
+    
+  </div>
+
+  <footer class=" bg-utama ">
+    <div class="container-fluid footerbawah footer-dibawah">
+      <p>Copyright @ 2020 - SIAKAD STMIK Sumedang</p>
+    </div>
+  </footer>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="js/jquery-3.5.1.slim.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/popper.min.js"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
