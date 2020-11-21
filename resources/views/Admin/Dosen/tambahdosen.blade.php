705c3c131df5432c39e307faa38c563a814e46f6
Ma'mur Mulyawan <mamurmulya@gmail.com>
2020-11-21 22:38:27 +0700

    create dosen

--- resources/views/Admin/Dosen/tambahdosen.blade.php
+++ resources/views/Admin/Dosen/tambahdosen.blade.php
@@ -15,81 +15,104 @@
                 <div class="card shadow">
                   <div class="card-header">Tambah Data Dosen</div>
                   <div class="card-body">
-                    <form action="">
+                    <form action="/dosen" method="post" enctype="multipart/form-data">
+                      @csrf
+                      @method('POST')
+
                       <div class="form-group row ">
                         <label for="" class="col-sm-3 col-form-label text-right">NIDN</label>
                         <div class="col-sm-9">
-                          <input type="text" name="#" class="form-control">
+                          <input type="text" name="nidn" class="form-control @error('nidn') is-invalid @enderror" value="{{ old('nidn') }}">
+                          @error('nidn') 
+                              <small class="text-danger">{{ $message }}</small>
+                          @enderror
                         </div>
                       </div>
   
                       <div class="form-group row">
                         <label for="" class="col-sm-3 col-form-label text-right">Nama Dosen</label>
                         <div class="col-sm-9">
-                          <input type="text" name="#" class="form-control">
+                          <input type="text" name="namadosen" class="form-control @error('namadosen') is-invalid @enderror" value="{{ old('namadosen') }}">
+                          @error('namadosen') 
+                              <small class="text-danger">{{ $message }}</small>
+                          @enderror
                         </div>
                       </div>
-  
+    
+                      <div class="form-group row">
+                        <label for="" class="col-sm-3 col-form-label text-right">Keilmuan</label>
+                        <div class="col-sm-9">
+                          <input type="text" name="keilmuan" class="form-control @error('keilmuan') is-invalid @enderror" value="{{ old('keilmuan') }}">
+                          @error('keilmuan') 
+                              <small class="text-danger">{{ $message }}</small>
+                          @enderror
+                        </div>
+                      </div>
+
                       <div class="form-group row">
                         <label for="" class="col-sm-3 col-form-label text-right">Tempat Lahir</label>
                         <div class="col-sm-9">
-                          <input type="text" name="#" class="form-control">
+                          <input type="text" name="tempat" class="form-control @error('tempat') is-invalid @enderror" value="{{ old('tempat') }}">
+                          @error('tempat') 
+                              <small class="text-danger">{{ $message }}</small>
+                          @enderror
                         </div>
                       </div>
   
                       <div class="form-group row">
                         <label for="" class="col-sm-3 col-form-label text-right">Tanggal Lahir</label>
                         <div class="col-sm-9">
-                          <input type="text" name="#" class="form-control">
+                          <input type="date" name="tgllahir" class="form-control @error('tgllahir') is-invalid @enderror" value="{{ old('tgllahir') }}">
+                          @error('tgllahir') 
+                              <small class="text-danger">{{ $message }}</small>
+                          @enderror
                         </div>
                       </div>
   
                       <div class="form-group row">
                         <label for="" class="col-sm-3 col-form-label text-right">No Handphone</label>
                         <div class="col-sm-9">
-                          <input type="text" name="#" class="form-control">
+                          <input type="text" name="telp" class="form-control @error('telp') is-invalid @enderror" value="{{ old('telp') }}">
+                          @error('telp') 
+                              <small class="text-danger">{{ $message }}</small>
+                          @enderror
                         </div>
                       </div>
-  
-                      <div class="form-group row">
-                        <label for="" class="col-sm-3 col-form-label text-right">Email</label>
+
+                       <div class="form-group row">
+                        <label for="" class="col-sm-3 col-form-label text-right">Password</label>
                         <div class="col-sm-9">
-                          <input type="text" name="#" class="form-control">
+                          <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" value="{{ old('password') }}">
+                          @error('password') 
+                              <small class="text-danger">{{ $message }}</small>
+                          @enderror
                         </div>
                       </div>
   
                       <div class="form-group row">
                         <label for="" class="col-sm-3 col-form-label text-right">Alamat</label>
                         <div class="col-sm-9">
-                          <input type="text" name="#" class="form-control">
+                          <input type="text" name="alamat" class="form-control @error('alamat') is-invalid @enderror" value="{{ old('alamat') }}">
+                          @error('alamat') 
+                              <small class="text-danger">{{ $message }}</small>
+                          @enderror
                         </div>
                       </div>
-  
-                      <div class="form-group row">
-                        <label for="" class="col-sm-3 col-form-label text-right">Kota</label>
-                        <div class="col-sm-9">
-                          <input type="text" name="#" class="form-control">
-                        </div>
-                      </div>
-  
-                      <div class="form-group row">
-                        <label for="" class="col-sm-3 col-form-label text-right">Kode POS</label>
-                        <div class="col-sm-9">
-                          <input type="text" name="#" class="form-control">
-                        </div>
-                      </div>
-  
+
                       <div class="form-group row">
                         <label for="" class="col-sm-3 col-form-label text-right">Gambar</label>
                         <div class="col-sm-9">
-                          <input type="file" name="gambar" class="form-control">
+                          <input type="file" name="gambar" class="form-control @error('gambar') is-invalid @enderror">
+                          @error('gambar') 
+                              <small class="text-danger">{{ $message }}</small>
+                          @enderror
                         </div>
                       </div>
   
                       <hr>
                       <div class="form-group">
                          <button type="submit" class="btn btn-primary mb-2">Simpan</button>
-                         <button type="submit" class="btn btn-danger mb-2">Keluar</button>
+                         <a href="/dosen" class="btn btn-danger mb-2">Keluar</a>
                       </div>
   
                     </form>
