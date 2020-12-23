a9287242cdd695e96d9ee6f51769a617adf4768f
Nunu Indra N <63348900+NunuIndra234@users.noreply.github.com>
2020-12-23 19:20:59 +0700

    Profile, edit profile, simpan edit na teu acan tiasa
    Co-Authored-By: Ma'mur Mulyawan <mamurmulya@gmail.com>
    Co-Authored-By: Robi-Rozali <52953946+Robi-Rozali@users.noreply.github.com>

--- resources/views/Mahasiswa/Profil/profil.blade.php
+++ resources/views/Mahasiswa/Profil/profil.blade.php
@@ -17,45 +17,47 @@
                   <div class="card-body">`
                       <div class="row">
                         <div class="col-md-4">
-                            <img src="/storage/gambar/{{ $mahasiswa->gambar }}" alt="" class="w-100">
+                            <img src="/storage/gambar/{{ Auth::guard('mahasiswa')->user()->gambar }}" alt="" class="w-100">
                         </div>
                         <div class="col-md-8">
                             <table class="table">
                                 <tr>
                                     <td>NIM</td>
-                                    <td><strong>{{ $mahasiswa->nim }}</strong></td>
+                                    <td><strong>{{ $mhs->nim }}</strong></td>
                                 </tr>
                                 <tr>
                                     <td>Nama</td>
-                                    <td><strong>{{ $mahasiswa->namadosen }}</strong></td>
+                                    <td><strong>{{ $mhs->nama }}</strong></td>
                                 </tr>
                                 <tr>
                                     <td>Tempat Lahir</td>
-                                    <td><strong>{{ $mahasiswa->tempat }}</strong></td>
+                                    <td><strong>{{ $mhs->tempat }}</strong></td>
                                 </tr>
                                 <tr>
                                     <td>Tanggal Lahir</td>
-                                    <td><strong>{{ $mahasiswa->tgllahir }}</strong></td>
+                                    <td><strong>{{ $mhs->tgllahir }}</strong></td>
                                 </tr>
                                 <tr>
                                     <td>Jurusan</td>
-                                    <td><strong>{{ $mahasiswa->jurusan }}</strong></td>
+                                    <td><strong>{{ $mhs->jurusan }}</strong></td>
                                 </tr>
                                 <tr>
                                     <td>No Handphone</td>
-                                    <td><strong>{{ $mahasiswa->telp }}</strong></td>
+                                    <td><strong>{{ $mhs->telp }}</strong></td>
                                 </tr>
                                 <tr>
                                     <td>Email</td>
-                                    <td><strong>{{ $mahasiswa->email }}</strong></td>
+                                    <td><strong>{{ $mhs->email }}</strong></td>
                                 </tr>
                             </table>
                         </div>
                   </div>
                     <hr>
                       <div class="form-group">
-                         <a href="/dosen" type="submit" class="btn btn-primary mb-2">Kembali</a>
+                         <a href="/mhs" type="submit" class="btn btn-primary mb-2">Kembali</a>
+                         <a href="/profil/{{ Auth::guard('mahasiswa')->user()->nim }}/edit" type="submit" class="btn btn-info mb-2">Edit</a>
                       </div>
+
                 </div>  
               </div>
               <div class="col-md-2"></div>
